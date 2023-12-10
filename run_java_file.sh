91ebb5b84fb6ab718a5aef2889c3990f1affd6ec
Alireza_rk107 <56473017+alirezark107@users.noreply.github.com>
2023-12-10 11:31:31 +0330

    Update run_java_file.sh
    add absolute addressing to script

--- run_java_file.sh
+++ run_java_file.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #this program give a java file as a argument and then compile and run it...
-javac $1
+
+
+javac $(echo $1|rev|cut -d "/" -f 1|rev)
 filename=$(echo $1|cut -d "." -f 1)
 java $filename
