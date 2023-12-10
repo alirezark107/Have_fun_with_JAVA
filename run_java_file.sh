835d77b8ee154da10f26c7177dda505082d5ba7c
Alireza_rk107 <56473017+alirezark107@users.noreply.github.com>
2023-12-10 12:44:19 +0330

    Update run_java_file.sh
    add another solution

--- run_java_file.sh
+++ run_java_file.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #this program give a java file as a argument and then compile and run it...
 
-
 javac $(echo $1|rev|cut -d "/" -f 1|rev)
+#or another solution for doing this :
+# javac $(echo $1|cut -d "/" -f $(echo ./javatest/test.java|grep -o "/" | wc -l))
+
 filename=$(echo $1|cut -d "." -f 1)
 java $filename
